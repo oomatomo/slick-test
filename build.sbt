523325c7bad86c23098d0e5bb13aab87d4de5faf
oomatomo <ooma0301@gmail.com>
2015-05-20 01:40:45 +0000

    custom codege

--- build.sbt
+++ build.sbt
@@ -6,15 +6,19 @@ scalaVersion := "2.11.6"
 
 // Change this to another test framework if you prefer
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "2.2.4" % "test"
-  // mysql connect
-  , "mysql" % "mysql-connector-java" % "5.1.28"
-  // slick
-  , "com.typesafe.slick" %% "slick" % "3.0.0"
-  , "com.typesafe.slick" %% "slick-codegen" % "3.0.0"
-  // Slickはデバックロギングのために利用する
-  , "org.slf4j" % "slf4j-nop" % "1.6.4"
-)
+    "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+    // mysql connect
+    , "mysql" % "mysql-connector-java" % "5.1.28"
+    // slick
+    , "com.typesafe.slick" %% "slick" % "3.0.0"
+    , "com.typesafe.slick" %% "slick-codegen" % "3.0.0"
+    // Slickはデバックロギングのために利用する
+    , "org.slf4j" % "slf4j-nop" % "1.6.4"
+    // For DB datetime
+    ,"joda-time" % "joda-time" % "2.7"
+    ,"org.joda" % "joda-convert" % "1.7"
+    ,"com.github.tototoshi" %% "slick-joda-mapper" % "2.0.0"
+    )
 //libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 
 // Uncomment to use Akka
--- src/main/scala/test/slick/CodeGen.scala
+++ src/main/scala/test/slick/CodeGen.scala
@@ -2,6 +2,10 @@ package test.slick
 
 import com.typesafe.config.{ Config, ConfigFactory }
 import slick.driver.JdbcProfile
+import slick.codegen.SourceCodeGenerator
+import slick.driver.MySQLDriver.simple._
+import slick.{ model => m }
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object CodeGen {
   def main(args: Array[String]) {
@@ -17,8 +21,40 @@ object CodeGen {
     val pkg          = "test.slick"
     val driver: JdbcProfile = scala.slick.driver.MySQLDriver
 
-    slick.codegen.SourceCodeGenerator.main(
-      Array(slickDriver, jdbcDriver, url, outputFolder, pkg, user, pass)
-    )
+    val db = {
+      driver.simple.Database.forURL(url, driver = jdbcDriver, user = user)
+    }
+    //slick.codegen.SourceCodeGenerator.main(
+    //  Array(slickDriver, jdbcDriver, url, outputFolder, pkg, user, pass)
+    //)
+
+    val modelAction = scala.slick.driver.MySQLDriver.createModel(Some(scala.slick.driver.MySQLDriver.defaultTables)) // テーブルのフィルタリングはここで行う
+    val modelFuture = db.run(modelAction)
+      // コードジェネレータをカスタマイズする
+    val codegenFuture = modelFuture.map(model => new SourceCodeGenerator(model) {
+      // マッピングするテーブルとクラス名をオーバーライド
+      //override def entityName =
+      //dbTableName => dbTableName.dropRight(1).toLowerCase.toCamelCase
+      //override def tableName =
+      //dbTableName => dbTableName.toLowerCase.toCamelCase
+      override def code = "import com.github.tototoshi.slick.MySQLJodaSupport._\n" + "import org.joda.time._\n" + super.code
+      override def Table = new Table(_){
+        override def Column = new Column(_){
+          override def rawType = model.tpe match {
+            case "java.sql.Timestamp" => "DateTime"
+            case "java.sql.Date"      => "LocalDate"
+            case _ => {
+              super.rawType
+            }
+          }
+        }
+        override def mappingEnabled : Boolean = false
+      }
+    })
+    codegenFuture.onSuccess { case codegen =>
+      codegen.writeToFile(
+        "slick.driver.MySQL","src/main/scala","test.slick","Tables","Tables.scala"
+      )
+    }
   }
 }
